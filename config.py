9a553cd696f50a9e37673dfd1a5cc5ab5f2cc2c6
Jposeph99 <joseph.ahmed65@yahoo.com>
2024-05-26 19:35:39 -0400

    email functionality isadded and being tested

--- config.py
+++ config.py
@@ -8,5 +8,13 @@ CORS(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydatabase.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+app.config['MAIL_SERVER'] = 'smtp.sendgrid.net'
+app.config['MAIL_PORT'] = 587  # Use SSL
+app.config['MAIL_USERNAME'] = 'apikey'
+app.config['MAIL_PASSWORD'] = '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'
+
+
+
+
 db = SQLAlchemy(app)
 # something 
--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-from flask import request, json, jsonify
+from flask import request, json, jsonify, render_template
 from config import app, db
 from models import Product
-
+from flask_mail import Mail, Message
+import os
+# from sendgrid import SendGridAPIClient
+# from sendgrid.helpers.mail import Mail
+import ssl
 
 # CRUD
 
 # Get Products
+
+
 @app.route('/product', methods=['GET'])
 def get_product():
     products = Product.query.all()
@@ -16,6 +22,8 @@ def get_product():
     })
 
 # Create product
+
+
 @app.route('/create-product', methods=['POST'])
 def create_products():
     product_name = request.json.get("productName")
@@ -29,48 +37,101 @@ def create_products():
     new_product = Product(product_name=product_name,
                           product_price=product_price,
                           product_img=product_img,
-                          product_quantity=product_quantity) 
+                          product_quantity=product_quantity)
     try:
         db.session.add(new_product)
         db.session.commit()
     except Exception as ex:
         return jsonify({"Message": str(ex)}), 400
-    
+
     return jsonify({"Message": "User created"}), 201
 
 
 # Update product
-@app.route("/update-product/<int:id>", methods = ["PATCH"])
+@app.route("/update-product/<int:id>", methods=["PATCH"])
 def update_product(id):
     product = Product.query.get(id)
 
     if not product:
         print('no product found')
-        return jsonify({"Message":"Product was not founded"}), 404
-    
+        return jsonify({"Message": "Product was not founded"}), 404
+
     data = request.json
     product.product_name = data.get("productName", product.product_name)
     product.product_price = data.get("productPrice", product.product_price)
     product.product_img = data.get("productImg", product.product_img)
-    product.product_quantity = data.get("productQuantity", product.product_quantity)
+    product.product_quantity = data.get(
+        "productQuantity", product.product_quantity)
 
     db.session.commit()
 
     return jsonify({"Message": "Product updated"}), 200
 
 # Delete product
-@app.route("/delete-product/<int:id>", methods = ["DELETE"])
+
+
+@app.route("/delete-product/<int:id>", methods=["DELETE"])
 def delete_product(id):
     product = Product.query.get(id)
 
     if not product:
         print('No product found')
         return jsonify({"Message": "Product was not founded to delete"}), 404
-    
+
     db.session.delete(product)
     db.session.commit()
 
     return jsonify({"Message": "Product was deleted successfully"}), 200
+
+
+mail = Mail(app)
+
+
+def send_confirmation(recipient):
+    msg = Message('Order Confirmation',
+                  sender='joseph.ahmed65@yahoo.com', recipients=[recipient])
+    # msg.html = render_template('index.html')
+    msg.body = "HELLLLLOPOOOO"
+    mail.send(msg)
+
+
+def send_notification(order, recipient):
+    msg = Message('Order Confirmation',
+                  sender='joseph.ahmed65@yahoo.com', recipients=[recipient])
+    # msg.html = render_template('index.html')
+    msg.body = "HELLLLLOPOOOO"
+
+    mail.send(msg)
+
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+
+@app.route('/place-order', methods=['POST'])
+def place_order():
+    # order = process_order(request.form)
+
+    # msg = Mail(
+    #     from_email='yousifm2099@gmail.com',
+    #     to_emails='zenb19@yahoo.com',
+    #     subject="welcoming",
+    #     html_content='<strong>hello is this working5555 inshalla</strong>'
+    # )
+    # sg = SendGridAPIClient(
+    #     "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")
+    # try:
+    #     res = sg.send(msg)
+    #     print(res.status_code)
+    #     print(res.body)
+    #     print(res.headers)
+    # except Exception as e:
+    #     print(e.message)
+    msg = Message('Confirmation', sender='help@dragonnier.com', recipients=['joseph.ahmed65@yahoo.com','yousifm2099@gmail.com'])
+    msg.html = '<strong class="style" >helllo world from css</strong>'
+    mail.send(msg)
+    
+    return 'Order submitted successfully'
+
 
 if __name__ == "__main__":
     with app.app_context():
